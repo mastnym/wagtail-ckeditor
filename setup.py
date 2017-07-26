ab21da546a03076fd0fee4232bd24d9f8e560084
Martin Mastny <mastnym@vscht.cz>
2017-07-26 10:58:42 +0200

    .gitignore + setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+   name='wagtail-ckeditor',
+   version='0.9',
+   description='CKEditor widget for Wagtail CMS',
+   author='Martin Mastny',
+   author_email='martin.mastny@vscht.cz',
+   packages=['wagtail_ckeditor'],
+   install_requires=['wagtail'],
+)
